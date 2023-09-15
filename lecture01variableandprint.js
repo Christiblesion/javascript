3d66a963f42bcc13b9ef6489ab934d319b091b00
Christiblesion <christiblesion@gmail.com>
2023-09-15 08:21:22 -0400

    code updated

---
+++ lecture01variableandprint.js
@@ -0,0 +1,28 @@
+
+document.write("GOOD AFTERNOON <br>")
+
+
+
+var studentname ="Christible soin;"
+document.write("students name <br> "+studentname)
+
+
+var classname= "lab02; <br>"
+document.write(" class name <br> ="+classname)
+
+var studentid= "01; <br>"
+document.write(" student id <br> ="+studentid)
+
+
+var teachername= "Sir Arsalan <br>"
+document.write(" class timing <br> ="+teachername)
+
+
+var classtiming= "5pm to 7pm <br>"
+document.write(" class timing <br> ="+classtiming)
+
+var coursename= " ADSE <br>"
+document.write(" course name <br> ="=+coursename)
+
+
+
